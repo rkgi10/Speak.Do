8dc68ee8d076a27ac6dbd17bdf584165b7dd7822
Rohit Gurnani <rkgi10@gmail.com>
2015-07-15 22:47:16 +0530

    Initial Commit

---
+++ SmartTasks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartTasks
+//
+//  Created by Rohit Gurnani on 15/07/15.
+//  Copyright (c) 2015 Rohit Gurnani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
