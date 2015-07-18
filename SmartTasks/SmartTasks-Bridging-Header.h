989106e905999f4473bc49cf40db65f278ea4d8f
Rohit Gurnani <rkgi10@gmail.com>
2015-07-18 19:24:04 +0530

    Added Wit.

---
+++ SmartTasks/SmartTasks-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import  <Wit/kiss_fft.h>
+#import <Wit/kiss_fftr.h>
+#import <Wit/Wit.h>
+#import <Wit/WITCircleLayer.h>
+#import <Wit/WITCircleView.h>
+#import <Wit/WITCvad.h>
+#import <Wit/WITMicButton.h>
+#import <Wit/WITRecorder.h>
+#import <Wit/WITVad.h>
+#import <Wit/WITVadConfig.h>
+#import <Wit/WITVadSimple.h>
