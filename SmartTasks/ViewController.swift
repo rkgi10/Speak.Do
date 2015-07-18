6104833d747a3f99f1dd4346e2d22d88a8c26436
Rohit Gurnani <rkgi10@gmail.com>
2015-07-18 16:12:26 +0530

    Changed the UI to have push segues

--- SmartTasks/ViewController.swift
+++ SmartTasks/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var navBarAppearance = UINavigationBar.appearance()
+        
+        navBarAppearance.barTintColor = UIColor(red: 103 / 255, green: 188 / 255, blue: 228 / 255, alpha: 1.0)
+        navBarAppearance.translucent = false
         // Do any additional setup after loading the view, typically from a nib.
     }
 
