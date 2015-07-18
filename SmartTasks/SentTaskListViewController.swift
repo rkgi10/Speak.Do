475f22b929edc42bc28c6769683bd50ceceed9ee
Rohit Gurnani <rkgi10@gmail.com>
2015-07-19 01:40:11 +0530

    added some features

---
+++ SmartTasks/SentTaskListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SentTaskListViewController.swift
+//  SmartTasks
+//
+//  Created by Rohit Gurnani on 19/07/15.
+//  Copyright (c) 2015 Rohit Gurnani. All rights reserved.
+//
+
+import UIKit
+
+class SentTaskListViewController: UIViewController , UITableViewDelegate , UITableViewDataSource{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+     let cell = tableView.dequeueReusableCellWithIdentifier("", forIndexPath: indexPath) as! UITableViewCell
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
